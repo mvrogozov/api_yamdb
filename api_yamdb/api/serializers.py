d3382cd225f3bf567620e71ee6ecdd5ec8654e86
lizakhud <hudainatova@uchi.ru>
2022-04-14 03:56:03 +0300

    add models and serializers

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from reviews.models import Title, Category, Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ("name", "slug")
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ("name", "slug")
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        fields = "__all__"
+        model = Title
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,33 @@ from django.db import models
 
 
 class Category(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
-    name = models.CharField(max_length=200),
-    slug = models.SlugField(max_length=200, unique=True)
+    def __str__(self):
+        return self.name
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre, related_name="titles")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="titles",
+    )
+
+    def __str__(self):
+        return self.name
